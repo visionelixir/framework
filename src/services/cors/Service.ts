a3c1c4a5f103398c57c3c4db82147ef55e3a225b
Brent Honeybone <brent.honeybone@gmail.com>
2021-04-14 17:53:16 +0200

    chore: add cors options to config

--- src/services/cors/Service.ts
+++ src/services/cors/Service.ts
@@ -1,13 +1,34 @@
-import { Service } from '../app/types'
+import {
+  AppType,
+  Service,
+  SERVICE_APP,
+  VisionElixirConfig,
+  VisionElixirJobConfig,
+} from '../app/types'
 import { Middleware } from '../core/types'
+import { App } from '../app/lib/App'
+import { Container } from '../container/types'
 import cors = require('@koa/cors')
 
 export default class CorsService implements Service {
-  public async middleware(middleware: Middleware[]): Promise<void> {
+  public async middleware(
+    middleware: Middleware[],
+    container: Container,
+  ): Promise<void> {
+    const app = container.resolve<App>(SERVICE_APP)
+
+    const config: VisionElixirConfig | VisionElixirJobConfig = {
+      ...app.getConfig(),
+    }
+
     Reflect.defineProperty(cors, 'name', {
       value: 'CorsMiddleware.cors',
     })
 
-    middleware.push(cors())
+    if (config.type === AppType.APP && config.cors) {
+      middleware.push(cors(config.cors))
+    } else {
+      middleware.push(cors())
+    }
   }
 }
